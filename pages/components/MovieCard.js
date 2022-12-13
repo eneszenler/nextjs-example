1061986b5f8977f95f8cda3d67c61b8facabc237
Yusuf Enes <91543268+eneszenler@users.noreply.github.com>
2022-12-14 02:25:01 +0300

    title updated

--- pages/components/MovieCard.js
+++ pages/components/MovieCard.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const MovieCard = ({item}) => {
   return (
     <Link href={`/movie/${item?.id}`}>
-        <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${item?.poster_path}`} alt={item.title} />
+        <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${item?.poster_path}`} alt={item?.title} />
         <h3>{item?.title}</h3>
     </Link>
   );
